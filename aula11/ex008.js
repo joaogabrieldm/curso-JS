a0db3f4558aa48cf1acadd8e004f65d82fd73947
João Gabriel <147347868+joaogabrieldm@users.noreply.github.com>
2023-11-16 18:14:11 -0300

    criação do ex008

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 100
+
+console.log(`Você está dirigindo a ${vel} Km/h`)
+if (vel > 50) {
+    console.log('DIMINUA A VELOCIDADE!!')
+}
+console.log('Não se esqueça do sinto de SEGURANÇA!')
