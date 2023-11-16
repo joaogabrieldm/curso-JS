4e911ac841d526e9001220cd96da5a9011e2dafb
João Gabriel <147347868+joaogabrieldm@users.noreply.github.com>
2023-11-16 18:24:36 -0300

    Criação do ex009

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'Alemanha'
+
+console.log(`Se você é nasceu no(a) ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
