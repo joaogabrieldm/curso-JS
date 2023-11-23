7d877529ac28bf1bd888bd9c3dfe23222cff1fb4
João Gabriel <147347868+joaogabrieldm@users.noreply.github.com>
2023-11-23 18:24:36 -0300

    Bug de cores

--- aula12.1/ex015/scripts/index.js
+++ aula12.1/ex015/scripts/index.js
@@ -9,29 +9,38 @@ function verificar() {
     
     if (idade > 500 || idade < 0) {
         res.innerHTML = `Opções inválidas!`
+        document.body.style.background = 'red'
     } else if (idade > 0 && idade <= 15 && sexM) {
         res.innerHTML = `Detectamos um MENINO de ${idade} anos`
         img.src = 'imagens/menino.png'
+        document.body.style.background = '#0084ff'
     } else if (idade > 15 && idade <= 21 && sexM) {
         res.innerHTML = `Detectamos um JOVEM de ${idade} anos`
         img.src = 'imagens/jovemM.png'
+        document.body.style.background = '#0084ff'
     } else if (idade > 21 && idade <= 65 && sexM) {
         res.innerHTML = `Detectamos um ADULTO de ${idade} anos`
         img.src = 'imagens/homem.jpg'
+        document.body.style.background = '#0084ff'
     } else if (idade > 65 && sexM){
         res.innerHTML = `Detectamos um SENHOR de ${idade} anos`
         img.src = 'imagens/idoso.jpg'
+        document.body.style.background = '#0084ff'
     }else if (idade > 0 && idade <= 15 && sexF) {
         res.innerHTML = `Detectamos uma MENINA de ${idade} anos`
         img.src = 'imagens/menina.png'
+        document.body.style.background = 'pink'
     } else if (idade > 15 && idade <= 21 && sexF) {
         res.innerHTML = `Detectamos uma JOVEM de ${idade} anos`
         img.src = 'imagens/jovemF.png'
+        document.body.style.background = 'pink'
     } else if (idade > 21 && idade <= 65 && sexF) {
         res.innerHTML = `Detectamos uma ADULTA de ${idade} anos`
         img.src = 'imagens/mulher.jpg'
+        document.body.style.background = 'pink'
     } else if (idade > 65 && sexF){
         res.innerHTML = `Detectamos uma SENHORA de ${idade} anos`
         img.src = 'imagens/idosa.jpg'
+        document.body.style.background = 'pink'
     }
 }
