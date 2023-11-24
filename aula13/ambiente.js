2c770c1bb3cf48cb6720355da6f15680ba7022f6
João Gabriel <147347868+joaogabrieldm@users.noreply.github.com>
2023-11-24 00:06:21 -0300

    Repetição com DO - WHILE

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,6 +1,14 @@
-var c = 1 
+/*var c = 1 
 
 while (c < 500) {
     c = c + 1
     console.log(`Passo ${c}`)
-}
+} */
+
+
+var c = 1
+
+do {
+    console.log(`Passo ${c}`)
+    c += 1
+} while (c <= 22)
