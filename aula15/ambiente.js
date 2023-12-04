126302410db39c03871772787d1efa34b8d47ff2
João Gabriel <147347868+joaogabrieldm@users.noreply.github.com>
2023-12-04 20:43:44 -0300

    Criação dos scripts

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,6,7,1,4,9]
+num[6] = 3
+num.push(2)
+
+
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições`)
+console.log(`O primeiro elemento é o ${num[0]}`)
---
+++ aula15/vetor-na-tela.js
@@ -0,0 +1,7 @@
+let num = [5,6,7,1,4,9]
+num[6] = 3
+num.push(2)
+
+for (let pos = 0; pos < num.length; ++pos){
+    console.log(num[pos])
+}
